fa7caba5c3f5582f22051f102c341d0e43bc2152
Enghel12 <enghel.bogdan@gmail.com>
2023-10-05 01:45:12 +0300

    Last version

--- EmployeeManagementSystem/Main.cpp
+++ EmployeeManagementSystem/Main.cpp
@@ -61,17 +61,6 @@ public:
 //Second project
 int main()
 {
-	/*This program does the following :
-	1.Create and save a list of employees
-	2.Printing the record, updating the record and deleting the record of each employee
-	and giving the user the possibility to find a specific records through his/her ID and Name
-	3.Dealocating the memory at the end of the execution of the program by using smart pointers
-	and the command delete and using the abstraction principle to determine if an employee
-	deserves a promotion
-	4.Use sorting algorithms to give the user the chance to change the order of the employees
-	records 
-	5.It also relies on user interface and file handling, OOP, and pointers and dinamic memory allocation
-	*/
 
 	//variables that we neeed for each record/unique object
 	int ID = 0, age = 0, salary = 0;
